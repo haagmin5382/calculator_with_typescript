a2de0c1b42b50444e66c91cbc9d5e58010667343
haagmin5382 <itworker5382@gmail.com>
2022-06-09 18:36:46 +0900

    feat : add calculator function

--- src/components/CalculatorButton.tsx
+++ src/components/CalculatorButton.tsx
@@ -18,13 +18,13 @@ const CalculatorButton = styled.button`
 
 const calculatorArr = [
   "C",
-  "/",
+  "=",
   <RiDeleteBack2Fill color="red" />,
-  "X",
+  "*",
   7,
   8,
   9,
-  "÷",
+  "/",
   4,
   5,
   6,
@@ -35,26 +35,44 @@ const calculatorArr = [
   "-",
   0,
   ".",
-
-  "=",
 ];
 
 interface Props {
   screen: string | undefined;
   setScreen: Dispatch<SetStateAction<string | undefined>>;
+
   //   setScreen: any;
 }
 
 const CalculatorButtons = ({ screen, setScreen }: Props) => {
+  // screen에 숫자가나오는 함수
   const clickButton = (e: any) => {
-    if (e.target.innerText === "C") {
-      setScreen("");
-    } else if (e.target.innerText === "" || e.target.innerText === undefined) {
+    // console.log(e.target.innerText);
+    if (isNaN(Number(e.target.innerText))) {
+      if (e.target.innerText === "C") {
+        setScreen("");
+      } else if (e.target.innerText === undefined) {
+        setScreen(screen?.slice(0, -1));
+      } else if (e.target.innerText === "=") {
+        let sum = new Function(`return ${screen}`);
+
+        setScreen(sum().toString());
+      } else {
+        if (screen) {
+          if (isNaN(Number(screen[screen.length - 1]))) {
+            setScreen((prevState) => prevState?.slice(0, -1));
+            setScreen((prevState) => prevState + e.target.innerText);
+          } else {
+            setScreen(screen + e.target.innerText);
+          }
+        }
+      }
+    } else if (e.target.innerText?.length === 0) {
       setScreen(screen?.slice(0, -1));
-    } else if (e.target.innerText === "=") {
     } else {
       setScreen(screen + e.target.innerText);
     }
+    console.log(screen);
   };
 
   return (
